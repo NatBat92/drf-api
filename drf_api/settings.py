e15de352bbbe7fa150f70e872a545bd38a9382a0
NatBat92 <natalie.neale1992@outlook.com>
2023-07-05 16:30:54 +0000

    add date/ time format to posts and comments

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -31,8 +31,16 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
@@ -52,7 +60,7 @@ SECRET_KEY = '9ylAEes1J3q0BPle9YzLVVsc6rU'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-natbat92-drfapi-q7qgj5vfrdp.ws-eu101.gitpod.io'    
+ALLOWED_HOSTS = ['8000-natbat92-drfapi-10gk5f4x90a.ws-eu101.gitpod.io'    
 ]
 
 
