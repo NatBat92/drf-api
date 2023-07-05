59e777231b75555723e49aa196fa2d087a547665
NatBat92 <natalie.neale1992@outlook.com>
2023-07-05 16:50:58 +0000

    add settings ready for heroku deployment

--- env.py
+++ env.py
@@ -1,3 +1,5 @@
 import os  
 os.environ['CLOUDINARY_URL'] = 'cloudinary://278915942242721:9ylAEes1J3q0BPle9YzLVVsc6rU@ddswglojm'
 os.environ['DEV'] = '1'
+os.environ['DATABASE_URL'] = 'postgres://jinatkun:bbTxqcQ5ko4bDnw6copcgG1dp8jA2Ipv@surus.db.elephantsql.com/jinatkun'
+os.environ.setdefault("SECRET_KEY", "Storm&Logan2022")
