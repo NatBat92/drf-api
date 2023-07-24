7454ba1cea8b0e41496bfe78bdf33ceb8fecdf74
NatBat92 <natalie.neale1992@outlook.com>
2023-07-24 08:39:46 +0000

    update posts app with review serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -13,6 +13,8 @@ class PostSerializer(serializers.ModelSerializer):
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
+    reviews_count = serializers.ReadOnlyField()
+    average_rating = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -47,4 +49,5 @@ class PostSerializer(serializers.ModelSerializer):
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter',
             'like_id', 'likes_count', 'comments_count' 'favourite_id',
+            'reviews_count', 'average_rating'
         ]
